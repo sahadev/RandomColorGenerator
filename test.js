1f1c5cf2517dcfda28981626d205c02c5b574612
Shangbin <shangbin@youhaodongxi.com>
2020-02-26 18:04:09 +0800

    精简代码

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const { generateColorTemplate, generateColor } = require('./lib/index.js');
 
-const pointer = generateColor(true, 4);
+const pointer = generateColor(true, 1);
 
 const resultArray = [];
-for (let index = 0; index < 1000; index++) {
+for (let index = 0; index < 10000; index++) {
     resultArray.push({ ID: `${index + 1}`, color: `${pointer.next().value}` });
 }
 
